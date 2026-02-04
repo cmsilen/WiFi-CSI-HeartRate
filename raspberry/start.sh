971f2a543522e20d369e159e9b1812c2ea2b0a3d
carlomazzanti <carlomazzanti00@gmail.com>
2026-02-04 18:43:58 +0100

    update script

--- raspberry/start.sh
+++ raspberry/start.sh
@@ -1,1 +1,10 @@
-sudo chrt -f 50 python hr_detection.py -p /dev/ttyUSB0 -ps /dev/ttyACM0
+#!/bin/bash
+
+# inizializza conda nella shell
+source ~/miniconda3/etc/profile.d/conda.sh
+
+# attiva l'ambiente
+conda activate iaproject
+
+# avvia python con priorità realtime
+exec sudo chrt -f 50 python hr_detection.py -p /dev/ttyUSB0 -ps /dev/ttyACM0
